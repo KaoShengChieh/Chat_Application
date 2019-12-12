a52e62d173b77925bea904bf7a347d3d1db8efbf
Ricky Kao <b06902117@ntu.edu.tw>
2019-12-12 12:50:06 +0800

    Handle socket IO for each connection/client

---
+++ Server/ClientHandler.java
@@ -0,0 +1,121 @@
+import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class ClientHandler implements Runnable {
+	private Socket socket;
+	private ObjectInputStream inputObject;
+	private ObjectOutputStream outputObject;
+	private List<ClientHandler> clientList;
+	private BlockingQueue<Packet> sendQueue;
+	private BlockingQueue<Packet> recvQueue;
+	public Client client;
+	
+	public ClientHandler(Socket socket, ObjectInputStream inputObject,
+		ObjectOutputStream outputObject, List<ClientHandler> clientList) { 
+		this.socket = socket;
+		this.inputObject = inputObject; 
+		this.outputObject = outputObject; 
+		clientList = clientList;
+		sendQueue = new BlockingQueue<>();
+		recvQueue = new BlockingQueue<>();
+		client = new Client(sendQueue, recvQueue, clientList);
+	}
+
+	public void run() {
+		try {
+			client.start();
+			
+			new Thread(new Runnable() { 
+				public void run() { 
+					Packet recv_packet = null;
+					try {
+						while (true) { 
+							recv_packet = readSocket();
+							setPacket(recv_packet);
+						}
+					} catch (IOException e) { 
+		            	e.printStackTrace();
+		            	close("Disconnected with server");
+		            } catch (ClassNotFoundException e) {
+		                e.printStackTrace();
+		                close("Fail to Serialized/Deserialized");
+		            }
+				} 
+			}).start();
+			
+			new Thread(new Runnable() { 
+				public void run() { 
+					Packet send_packet = null;
+					try {
+						do {
+							send_packet = getPacket();
+							writeSocket(send_packet);
+						} while (send_packet.type != Packet.Type.QUIT);
+					} catch (IOException e) { 
+		                e.printStackTrace();
+		                close("Disconnected with server");
+		            }
+				}
+			}).start();
+			
+			synchronized (client) {
+				while (client.isQuit() == false) {
+					try {
+						wait();
+					} catch (InterruptedException e)  {
+						Thread.currentThread().interrupt(); 
+					}
+				}
+			}
+			
+			close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			close("Unexpected error");
+		}
+	}
+	
+	private Packet readSocket() throws IOException, ClassNotFoundException {
+		return (Packet)inputObject.readObject();
+	}
+	
+	private void writeSocket(Packet packet) throws IOException {
+		outputObject.writeObject(packet);
+		outputObject.flush();
+	}
+	
+	private Packet getPacket() {
+		return sendQueue.pop();
+	}
+	
+	private void setPacket(Packet packet) {
+		recvQueue.push(packet);
+	}
+	
+	private void close() {
+		try {
+			if (inputObject != null) {
+				inputObject.close();
+			}
+			if (outputObject != null) {
+				outputObject.close();
+			}
+			if (socket != null) {
+				socket.close();
+			}
+		} catch (IOException e) {
+			System.err.println("Socket close error");
+		} finally {
+			System.out.println("Connection with " + (client.userID() >= 0 ? "unknown user" : "user " + client.userID()) + " closed");
+			clientList.remove(this);
+		}
+	}
+	
+	private void close(String errMessage) {
+		System.err.println(errMessage);
+		close();
+	}
+}

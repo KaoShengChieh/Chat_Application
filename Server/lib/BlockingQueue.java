564b6df9b0d846ff7020fd4c087aaa8ea88b85d6
Ricky Kao <b06902117@ntu.edu.tw>
2019-12-12 12:43:51 +0800

    Asynchronous working queue for socket I/O

---
+++ Server/lib/BlockingQueue.java
@@ -0,0 +1,31 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class BlockingQueue <T>
+{
+	private Queue<T> queue = new LinkedList<>();
+ 	
+ 	@SuppressWarnings (value="unchecked")
+	
+	public synchronized void push(T t) {
+		try {
+			queue.add((T)(t.getClass().getMethod("clone").invoke(t)));
+		} catch (Exception e) {
+			throw new RuntimeException("Clone not supported", e);
+		}
+		
+		notifyAll();
+	}
+	
+	public synchronized T pop() {
+		while (queue.size() == 0) {
+			try {
+				wait();
+			} catch (InterruptedException e)  {
+				Thread.currentThread().interrupt(); 
+			}
+		}
+		
+		return queue.poll();
+	}
+}

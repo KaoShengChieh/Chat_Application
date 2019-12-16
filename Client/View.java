ebb8741439bf7bc4bd0a957c4e6c978fae06ceef
Ricky Kao <b06902117@ntu.edu.tw>
2019-12-16 16:32:52 +0800

    Control interface for view of GUI

--- Client/View.java
+++ Client/View.java
@@ -1,4 +1,8 @@
 public interface View {
+	/**********************************************
+	 * This file is for reference only, you are   *
+	 * not required to follow the implementation. *
+	 **********************************************/
 	void getOffline();
 	void newMessage(Message message);
 	void newFriend(User friend);
